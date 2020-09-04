abc026703befe4bde609d43f6c9da062e43bd281
Jordan Burke <jordan.burke@gmail.com>
2020-09-04 16:02:34 -0400

    Helio Changes.

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TAG=3.0.0-hadoop3.2
+TAG=2.4.6
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=jordanburke/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -16,7 +16,7 @@ build() {
 build base
 build master
 build worker
-build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+#build submit
+#build java-template template/java
+#build scala-template template/scala
+#build python-template template/python
